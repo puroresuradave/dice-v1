91343a2680796441d73b608f068f0353642c0671
puroresuradave <puroresuradave@gmail.com>
2025-07-22 16:41:06 -0700

    Add files via upload

--- src/dmdice/app.py
+++ src/dmdice/app.py
@@ -13,122 +13,124 @@ class DMDice(toga.App):
 
         main_box = toga.Box()
 
-        box1 = toga.Box(direction=COLUMN, margin=10, flex=1)
-        box1.add(toga.Button(
+        leftContent = toga.Box(style=Pack(direction=COLUMN))
+
+        leftContent.add(toga.Button(
             "5e Character",
             on_press=self.button1,
             style=Pack(margin=10)
         ))
-        box1.add(toga.Button(
+        leftContent.add(toga.Button(
             "Dungeon Layout",
             on_press=self.button2,
             style=Pack(margin=10)
         ))
-        box1.add(toga.Button(
+        leftContent.add(toga.Button(
             "Loot",
             on_press=self.button3,
             style=Pack(margin=10)
         ))
-        box1.add(toga.Button(
+        leftContent.add(toga.Button(
             "Trap",
             on_press=self.button4,
             style=Pack(margin=10)
         ))
-        box1.add(toga.Button(
+        leftContent.add(toga.Button(
             "NPC Disposition",
             on_press=self.button5,
             style=Pack(margin=10)
         ))
-
-        box2 = toga.Box(direction=COLUMN, margin=10, flex=1)
-        box2.add(toga.Button(
+        leftContent.add(toga.Button(
             "Weather",
             on_press=self.button6,
             style=Pack(margin=10)
         ))
-        box2.add(toga.Button(
+        leftContent.add(toga.Button(
             "Encounter",
             on_press=self.button7,
             style=Pack(margin=10)
         ))
-        box2.add(toga.Button(
+        leftContent.add(toga.Button(
             "Fumble Melee",
             on_press=self.button8,
             style=Pack(margin=10)
         ))
-        box2.add(toga.Button(
+        leftContent.add(toga.Button(
             "Fumble Bow",
             on_press=self.button9,
             style=Pack(margin=10)
         ))
-        box2.add(toga.Button(
+        leftContent.add(toga.Button(
             "Fumble Thrown",
             on_press=self.button10,
             style=Pack(margin=10)
         ))
 
-        box3 = toga.Box(direction=COLUMN, margin=10, flex=1)
-        box3.add(toga.Button(
+        rightContent = toga.Box(style=Pack(direction=COLUMN))
+        
+        rightContent.add(toga.Button(
             "General Crit",
             on_press=self.button11,
             style=Pack(margin=10)
         ))
-        box3.add(toga.Button(
+        rightContent.add(toga.Button(
             "Arrow Crit",
             on_press=self.button12,
             style=Pack(margin=10)
         ))
-        box3.add(toga.Button(
+        rightContent.add(toga.Button(
             "Bludgeoning Crit",
             on_press=self.button13,
             style=Pack(margin=10)
         ))
-        box3.add(toga.Button(
+        rightContent.add(toga.Button(
             "Piercing Crit",
             on_press=self.button14,
             style=Pack(margin=10)
         ))
-        box3.add(toga.Button(
+        rightContent.add(toga.Button(
             "Slashing Crit",
             on_press=self.button15,
             style=Pack(margin=10)
         ))
-
-        box4 = toga.Box(direction=COLUMN, margin=10, flex=1)
-        box4.add(toga.Button(
+        rightContent.add(toga.Button(
             "Unarmed Crit",
             on_press=self.button16,
             style=Pack(margin=10)
         ))
-        box4.add(toga.Button(
+        rightContent.add(toga.Button(
             "Vorpal Weapon Crit",
             on_press=self.button17,
             style=Pack(margin=10)
         ))
-        box4.add(toga.Button(
+        rightContent.add(toga.Button(
             "Cursed Weapon Crit",
             on_press=self.button18,
             style=Pack(margin=10)
         ))
-        box4.add(toga.Button(
+        rightContent.add(toga.Button(
             "PC Crit on Dragon",
             on_press=self.button19,
             style=Pack(margin=10)
         ))
-        box4.add(toga.Button(
+        rightContent.add(toga.Button(
             "Dragon Crit on PC",
             on_press=self.button20,
             style=Pack(margin=10)
         ))
 
+        leftContainer = toga.ScrollContainer(flex=1)
+        leftContainer.content = leftContent
 
-        main_box.add(box1)
-        main_box.add(box2)
-        main_box.add(box3)
-        main_box.add(box4)
+        rightContainer = toga.ScrollContainer(flex=1)
+        rightContainer.content = rightContent
+
+        split = toga.Box(style=Pack(direction=ROW))
+        split.add(leftContainer)
+        split.add(rightContainer)
 
         self.main_window = toga.MainWindow(title=self.formal_name)
-        self.main_window.content = main_box
+        self.main_window.content = split
         self.main_window.show()
 
 
